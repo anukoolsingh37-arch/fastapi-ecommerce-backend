2afe40f2b316f021f146ef05ac5ef329ec9d493b
anukoolsingh37 <anukoolsingh37@gmail.com>
2026-05-14 13:27:08 +0530

    Redirect root to storefront on Render

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from database import engine
 from routers import products
 from routers import user
@@ -33,9 +33,8 @@ def store():
         return f.read()
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def home():
-    with open("static/index.html", "r", encoding="utf-8") as f:
-        return f.read()
+    return RedirectResponse(url="/store")
 
 
